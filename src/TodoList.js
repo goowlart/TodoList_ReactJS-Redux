3b3a73cfe1729fd02c3fc6f38965eeace9ca6d2a
Alex Goulart <goowlart@gmail.com>
2018-11-07 14:51:00 +0100

    first commit

---
+++ src/TodoList.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as todoActios from './actions/todos';
+
+
+class TodoList extends Component {
+    constructor(props) {
+      super (props);
+
+      console.log(props);
+    }
+
+    state = {
+    addNewTodoText: '',
+    };
+
+    addNewTodo = () => {
+       this.props.addTodo( this.state.addNewTodoText);
+
+       this.setState({addNewTodoText: ''});
+    };
+
+    render() {
+        return (
+            <div>  
+                <ul>
+                  { this.props.todos.map(todo => (
+                      <li key={todo.id}>{todo.text}</li>
+                  )) }
+                </ul>   
+         
+             <input type='text' 
+             value={this.state.addNewTodoText}
+             onChange={(e) => this.setState({ addNewTodoText: e.target.value })}
+             /> 
+
+             <button onClick={this.addNewTodo}>Novo Todo</button>
+             </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+ todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(todoActios, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
---
+++ src/reducers/todos.js
@@ -0,0 +1,17 @@
+export default function todos(state = [], action) {
+    // { type: 'ADD_TODO', text: 'Fazer café' }
+
+    switch (action.type) {
+        case 'ADD_TODO':
+            // addiciona novo todo 
+            return [ ...state, {
+                id: Math.random(),
+                text: action.text,
+            }]
+        default:
+          return state;
+        }
+    
+}
+
+//{todos: [],}
